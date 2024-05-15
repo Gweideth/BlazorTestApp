8504c7ca9b06f9a6f9d16774f26ebc9c9f1d12de
Filip Zolnierowicz <filipzolnierowicz@gmail.com>
2024-05-15 19:37:18 +0200

    Dodaj pliki projektów.

---
+++ BlazorTest/Models/ButtonModel.cs
@@ -0,0 +1,13 @@
+﻿using MudBlazor;
+
+
+namespace BlazorTest.Models;
+
+public class ButtonModel
+{
+    public int Id { get; set; } = 0;
+    public Dictionary<string, string> ButtonDefinition { get; set; }
+}
+
+
+
